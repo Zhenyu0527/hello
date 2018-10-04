d6e70ad58aa38264574e00cda58b972e899342cd
许震宇 <wwwxuzhenyu@126.com>
2018-10-04 10:58:49 +0800

    Initial Commit

---
+++ hello/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hello
+//
+//  Created by 许震宇 on 2018/10/4.
+//  Copyright © 2018年 许震宇. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
